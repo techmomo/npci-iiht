9bbdd5bdac36bd358d107f85135da11b113cb9a8
techmomo <mkhursheed@spireon.com>
2021-09-17 10:12:59 +0530

    Ds Algo Intro

---
+++ hello-world/src/com/mohsinkd786/dsalgo/Main.java
@@ -0,0 +1,78 @@
+package com.mohsinkd786.dsalgo;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+/*
+* Data Structures:
+*
+*   1. Linear
+*           a. Array
+*           b. Stack
+*           c. Queue
+*           d. LinkedList
+*   2. Non Linear
+*           a. Graph
+*           b. Tree
+*
+*
+*
+*   Operations in a Data Structure:
+*
+*   Asymptotic Notations:
+*   1. Big O - Upper Bound - worst case complexity / performance
+*           a. constant time - order of 1 = O(1)
+*           b. linear time - order of n = O(n)
+*           c. quadratic time - order of n times = O(n2)
+*           d. log time - O(log n)
+*           e. linear log time - O(n log n)
+*
+*   2. Theta - Middle / Average Bound - average
+*   3. Omega - Lower Bound - best case
+*
+*
+*   if(){
+*
+* }
+* n = 2
+* O(2)
+* n = 100
+* O(100)
+* O(n)
+* O(n) * O(m) = O(n * n)
+*
+* for(int i=0;i<n;i++){
+*    for(int j =0;j<m ; j++){
+*
+*   }
+* }
+*
+*   find(){
+*
+*       while(low <= high){
+*
+*           mid = (low + high) / 2
+*           if(){
+*               high = ?
+*           }else if(){
+*               low = ?
+*           }
+*           else{
+*           break;
+*           }
+*       }
+*   }
+*
+*   find(){
+*       find();
+*       find();
+* }
+*
+*
+* Assignment : Write a Program that arranges the elements in an array in ascending order.
+* */
